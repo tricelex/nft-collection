9394f9c72223b17c65471b1e998615d3968eca02
emmanuelokoye <emmanuelokoye@quickcheck.ng>
2022-04-20 13:20:30 +0100

    Added NFT Contract

---
+++ hardhat-contracts/constants/index.ts
@@ -0,0 +1,4 @@
+export const WHITELIST_CONTRACT_ADDRESS =
+  "0x7c03EfaD4808C833CEaACE8A841e3fA82B87bb8d";
+
+export const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
--- hardhat-contracts/scripts/deploy.ts
+++ hardhat-contracts/scripts/deploy.ts
@@ -4,6 +4,7 @@
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
 import { ethers } from "hardhat";
+import { WHITELIST_CONTRACT_ADDRESS, METADATA_URL } from "../constants/index";
 
 async function main() {
   // Hardhat always runs the compile task when running scripts with its command
@@ -13,13 +14,19 @@ async function main() {
   // manually to make sure everything is compiled
   // await hre.run('compile');
 
+  // Address of the whitelist contract that you deployed in the previous module
+  const whitelistContract = WHITELIST_CONTRACT_ADDRESS;
+  // URL from where we can extract the metadata for a Crypto Dev NFT
+  const metadataURL = METADATA_URL;
+
   // We get the contract to deploy
-  const Greeter = await ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
 
-  await greeter.deployed();
+  const CryptoDevs = await ethers.getContractFactory("CryptoDevs");
+  const cryptoDevs = await CryptoDevs.deploy(metadataURL, whitelistContract);
 
-  console.log("Greeter deployed to:", greeter.address);
+  await cryptoDevs.deployed();
+
+  console.log("CryptoDevs deployed to:", cryptoDevs.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
